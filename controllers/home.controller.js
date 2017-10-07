12aa8cfa0baa273b034f4a1ddaa460fa636f5d3a
sarochamo <32552067+sarochamo@users.noreply.github.com>
2017-10-07 16:19:09 +0700

    add content to twig

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,3 +1,28 @@
 exports.index = function(req,res) {
-	res.render('home.twig')
+
+	let content = {
+		highlight: {
+			url:'/assets/images/home/banner/highlight.jpg',
+			title: 'Hello'
+		},
+		thumbnail:[
+		{
+			url:'/assets/images/home/banner/thumbnail-1.jpg',
+			title: 'SARIO'
+		},
+		{
+			url:'/assets/images/home/banner/thumbnail-2.jpg',
+			title: 'DORAEMON'
+		},
+		{
+			url:'/assets/images/home/banner/thumbnail-3.jpg',
+			title: 'MARUKO'
+		},
+		{
+			url:'/assets/images/home/banner/thumbnail-4.jpg',
+			title: 'HAMTARO'
+		}
+		]
+	}
+	res.render('home.twig', content)
 }
